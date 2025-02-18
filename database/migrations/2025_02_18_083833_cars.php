1092059d5933f03c672baf3ec6d6c0a71d227358
jcarrozadev <jariasc14@gmail.com>
2025-02-18 10:35:14 +0100

    Added Migrations Cars + Gallery

---
+++ database/migrations/2025_02_18_083833_cars.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement()->primary();
+            $table->string('name', 100);
+            $table->integer('brand_id');
+            $table->integer('type_id');
+            $table->integer('color_id');
+            $table->year('year');
+            $table->string('main_img', 50);
+            $table->decimal('horsepower', 5, 2);
+            $table->boolean('sale');
+            $table->boolean('enabled');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->foreign('color_id')->references('id')->on('colors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
