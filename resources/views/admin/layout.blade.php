d187c85bad05088237d86e488c74ff0446942fdc
jcarrozadev <jariasc14@gmail.com>
2025-02-20 12:13:47 +0100

    Fix Folders Views Car.JS y Admin.JS

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -26,7 +26,7 @@
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
-
+    <script src="{{ asset('js/admin.js') }}"></script>
     @stack('js')
 </body>
 </html>
